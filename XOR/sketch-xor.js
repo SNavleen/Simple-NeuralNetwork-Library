a75e6088a2e384f54f57aed9aa897793a5dafef0
Navleen <navleen95@gmail.com>
2018-10-27 22:39:33 -0400

    show the learning using mini batches

--- XOR/sketch-xor.js
+++ XOR/sketch-xor.js
@@ -20,27 +20,33 @@ const trainingData = [
 
 function setup(){
   // createCanvas must be the first statement
-  createCanvas(400, 400);  
+  createCanvas(400, 400);
   
   nn = new NeuralNetwork(2, 2);
   nn.addHiddenLayer(2);
   nn.generateWeights(0);
-  for(let k = 0; k < 10000; k++){
-    var data = random(trainingData);
-    let input = data.input;
-    let output = data.output;
-    
-    outputPrim = nn.guess(JSON.parse(JSON.stringify(input)));
-    cost = nn.getCost(output, outputPrim);
-    nn.train(cost, 0.3);
-  }
 }
 
 function draw(){
   background(0);
+  var totalCost = [0, 0];
+  for(let k = 0; k < 1; k++){
+    var data = random(trainingData);
+    let input = data.input;
+    let output = data.output;
+    
+    var outputPrim = nn.guess(JSON.parse(JSON.stringify(input)));
+    var cost = nn.getCost(output, outputPrim);
+    totalCost[0] = totalCost[0] + cost[0];
+    totalCost[1] = totalCost[1] + cost[1];
+    // console.log(cost);
+  }
+  nn.train(totalCost, 0.3);
+
   let resolution = 10;
   let col = width / resolution;
   let row = height / resolution;
+
   for(i = 0; i < col; i++){
     for(j = 0; j < row; j++){
       let x = [i / col, j / row];
