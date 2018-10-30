c5bd00d0f8ed16b52cc61ed4b1743f6459bd5070
Navleen <navleen95@gmail.com>
2018-10-30 18:06:01 -0400

    Added basic training properties

--- ColorPredictor/sketch-colorPredictor.js
+++ ColorPredictor/sketch-colorPredictor.js
@@ -9,7 +9,32 @@ function randomColor(){
 }
 
 function mousePressed(){
-  randomColor();
+
+  let guess = prediction(red, green, blue);
+
+  if(mouseX >  0 && mouseX < width / 2){
+    // White (aka 1)
+    // Get cost
+    var cost = nn.getCost(1, guess);
+
+    // Train
+    nn.train(cost);
+
+    // New example
+    randomColor();
+    redraw();
+  }else if(mouseX >  width / 2 && mouseX < width){
+    // White (aka 1)
+    // Get cost
+    var cost = nn.getCost(0, guess);
+
+    // Train
+    nn.train(cost);
+
+    // New example
+    randomColor();
+    redraw();
+  }
 }
 
 function prediction(r, g, b){
@@ -21,7 +46,7 @@ function prediction(r, g, b){
 
   let input = [red, green, blue];
   // console.log(input);
-  let output = nn.guess(input);
+  let output = nn.guess(JSON.parse(JSON.stringify(input)));
 
   // return output;
   return Math.round(output[0]);
@@ -30,16 +55,24 @@ function prediction(r, g, b){
 function setup(){
   // createCanvas must be the first statement
   createCanvas(500, 500);
-  randomColor();
+  noLoop();
+
   nn = new NeuralNetwork(3, 1);
   nn.addHiddenLayer(3);
   nn.addHiddenLayer(2);
   nn.setLearningRateAlpha(0.1);
-  nn.generateWeights(1);
+  nn.generateWeights(0);
+
+  randomColor();
 }
 
 function draw(){
   background(0);
+
+  strokeWeight(4);
+  stroke(255);
+  line(width / 2, 0, width / 2, height);
+
   noStroke();
 
   let circleWidth = width / 3;
@@ -57,9 +90,9 @@ function draw(){
   text("WHITE", cirlceX, circleY);
   fill(0);
   text("BLACK", width - cirlceX, circleY);
-  
-  let guess = prediction(red, green, blue)
-  // console.log(guess);
+
+  let guess = prediction(red, green, blue);
+  console.log(guess);
   fill(255);
   if(guess == 1){
     ellipse(cirlceX, circleY / 2, circleWidth / 5, circleHeight / 5);
